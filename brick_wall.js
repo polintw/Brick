550917fbbb86a670a955a4ff02b91ed7038ce16e
b8208241 <b8208241@gmail.com>
2016-09-16 17:08:49 +0800

    9/16 完成至能有網頁title，點擊會有燈箱效果

--- brick_wall.js
+++ brick_wall.js
@@ -1,3 +1,15 @@
+function drag(ev) {
+    //target the <a> tag which wrapping the "brick"<div>
+	ev.dataTransfer.setData("a_brick", ev.target.parentElement.id);
+}
+
+function dragleave_handler(ev) {
+    ev.preventDefault();
+	if (ev.target.classList.contains("dragover")){
+    ev.target.classList.remove("dragover");
+	}
+}
+
 function allowDrop(ev) {
     ev.preventDefault();
     if (ev.target.classList.contains("placeholder")){
@@ -8,39 +20,28 @@ function allowDrop(ev) {
 	}
 }
 
-function dragleave_handler(ev) {
-    ev.preventDefault();
-	if (ev.target.classList.contains("dragover")){
-    ev.target.classList.remove("dragover");
-	}
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("a_brickOriginal", ev.target.id);
-}
-
 function drop(ev) {
     ev.preventDefault();ev.stopPropagation();
-	var brickOriginal = document.getElementById(ev.dataTransfer.getData("a_brickOriginal"));
-	var source_cell = brickOriginal.parentElement;
+	var brick = document.getElementById(ev.dataTransfer.getData("a_brick"));
+	var source_cell = brick.parentElement;
 	var source_row = source_cell.parentElement;
 	var row = ev.target.parentElement;
 	if (! row.classList.contains("row")){return false};
 	//drop a brickOriginal into a new position
     if (ev.target.classList.contains("cell") && ev.target.getElementsByClassName('brickOriginal')[0] === undefined){
-        ev.target.appendChild(brickOriginal);
+        ev.target.appendChild(brick);
         ev.target.classList.remove("dragover");
     }
 	else if (ev.target.classList.contains("placeholder")){
 		var cell = document.createElement("div");
 		cell.classList.add("cell");
-		cell.appendChild(brickOriginal);
+		cell.appendChild(brick);
 		row.insertBefore(cell,ev.target);
 	}
 	// rearrange the original row
 	rearrange(row);
 	if (3 < source_row.getElementsByClassName("cell").length &&
-		source_cell.getElementsByClassName("brickOriginal")[0] == undefined){
+		source_cell.getElementsByClassName("brickOriginal")[0] === undefined){
 		var blank = document.createElement("div");
 		source_cell.appendChild(blank);
 		blank.classList.add("brickOriginal-Blank");
@@ -72,33 +73,54 @@ function rearrange(row_){
     }
 } */
 
-function initialize(){
-    document.addEventListener('drop', drop);
-    document.addEventListener('dragover', allowDrop);
-    document.addEventListener('dragleave', dragleave_handler);
-    // document.addEventListener('dragend', dragend);
-}
 
 function add_brickOriginal (){
     var text = document.getElementById('main_text').value
     text = text.replace(/\n/g, '<br/>').replace(/ /g, '&nbsp;');
-    /*var ref = document.getElementById('ref').value*/
+	var ref = document.getElementById('ref').innerHTML;
     if (text.length < 1){return 0;}
     var cells = document.getElementsByClassName('cell');
     for(let i = cells.length - 1; i >= 0; i--){
-        if (cells[i].getElementsByClassName('brickOriginal')[0]){
+		if (cells[i].getElementsByClassName('brickOriginal')[0]){
             continue;
 		}else{
-            cells[i].innerHTML += ('<div id="brickOriginal' + document.getElementsByClassName('brickOriginal').length + '" class="brickOriginal" draggable="true" ondragstart="drag(event);" ondragend="dragend(event);" >'
+			cells[i].innerHTML += (
+			//wrap the "brick" with a anchor tag, for linking colorbox
+			'<a id="anchor_brickOriginal' + document.getElementsByClassName('brickOriginal').length + '" href="1402871283927540223.jpg">' 
+			//then create the brick style`
+			+ '<div id="brickOriginal' + document.getElementsByClassName('brickOriginal').length + '" class="brickOriginal" draggable="true" ondragstart="drag(event);" ondragend="dragend(event);">'
+			//then pull in the main_text and reference
             + '<p class="brick-content">' + text + '</p>'
-            /*+ '<p class="brick-ref">' + ref + '</p>'*/
-            + '</div>')
-            break;
+			+ '<p class="brick-ref">' + ref + '</p>'
+            + '</div>' 
+			+ '</a>');
+			//link the anchor tag to the colorbox effect
+			var newAnchor = cells[i].getElementsByTagName('a')[0];
+			$(newAnchor).colorbox();
+			break;
         }
     }
     document.getElementById('main_text').value = null;
-    /*document.getElementById('ref').value = null;*/
+    document.getElementById('ref').innerHTML = null;
 }
 
+function add_Title(event){
+	var html = event.clipboardData.getData('text/html');
+	document.getElementById('pasteHtml').innerHTML = html;
+	var string = document.getElementById('pasteHtml').value;
+	var stringUppercase = string.toUpperCase();
+	var positionStart = stringUppercase.search(/<title>/i);
+	var positionEnd = stringUppercase.search('</TITLE>');
+	var title = string.slice(positionStart+7,positionEnd);
+	document.getElementById('ref').innerHTML = title;
+}
+
+function initialize(){
+    document.addEventListener('drop', drop);
+    document.addEventListener('dragover', allowDrop);
+    document.addEventListener('dragleave', dragleave_handler);
+	document.getElementById('main_text').addEventListener('paste', add_Title);
+    // document.addEventListener('dragend', dragend);
+}
 
 window.onload = initialize;
